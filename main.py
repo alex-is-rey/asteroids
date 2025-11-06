af45245595811927683e6ee11069150355baa875
alex-is-rey <aguilar.alexis95@gmail.com>
2025-11-05 20:25:36 -0600

    added asteroid, asteroidfield, circleshape, logger, and player files. modified constants, .gitignore, and main

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import pygame
+from asteroidfield import *
 from constants import *
+from asteroid import *
+from player import *
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroidfield = AsteroidField()
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-    screen.fill(0)
-    pygame.display.flip()
+                running = False
+        screen.fill((0, 0, 0))
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+    pygame.quit()
 
 
 if __name__ == "__main__":
